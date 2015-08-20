94113ab5449acec4f6e7c372f3857363b2bbb214
kamilkazmierczak <kamil.kazmierczak@hotmail.com>
2015-08-20 21:16:38 +0200

    zmiany

--- Doom/main.cpp
+++ Doom/main.cpp
@@ -6,17 +6,6 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-
-	//nowe zmiany
-	
-	
-	cout << "Co tam ciekawego !!" << endl;
-	//wiecej zmian
-	//<ciczior>
-
-	//WIELKIE ZMIANY
-	//!!
-
 	GameManager *gameManager = &GameManager::getGameManager();
 	gameManager->runGameLoop();
 	GameManager::destroyGameManager();
