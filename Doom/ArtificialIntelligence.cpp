72185fd4b3f5ef88f666c0116fdc0a82f5d6b016
kamilkazmierczak <kamil.kazmierczak@hotmail.com>
2015-09-19 05:40:52 +0200

    +AI

--- Doom/ArtificialIntelligence.cpp
+++ Doom/ArtificialIntelligence.cpp
@@ -23,12 +23,63 @@ void ArtificialIntelligence::move(Entity* entity, GLfloat speed)
 		//go somewhere else
 		moveSomewhere(entity, speed);
 	}
+
+	if (rand() % 100 + 1 <5)
+	{
+		shoot(entity);
+	} 
+
 }
 
+
+void ArtificialIntelligence::shoot(Entity* entity)
+{
+	ResourceManager *resourceManager = &ResourceManager::getResourceManager();
+	RenderSystem *renderSystem = &RenderSystem::getRenderSystem();
+	CameraSystem *cameraSystem = &CameraSystem::getCameraSystem();
+
+
+	vec3 direction = vec3(entity->getMovementDirection().x, entity->getMovementDirection().y, entity->getMovementDirection().z);
+	//tworzenie naboju 
+	vec3 center = vec3(entity->getMovementDirection().x, entity->getMovementDirection().y, entity->getMovementDirection().z);
+												//te 0.2 nie dziala idealnie bo teraz strzela z roznych miejsc
+												//ale lepiej to wyglada niz ze srodka
+												//jak bd czas zrob zeby zawsze strzelal z swojej broni
+	vec3 position = vec3(entity->getPosition().x + 0.2f, entity->getPosition().y + 0.7f, entity->getPosition().z);
+
+	//testy
+	position = position + 0.7f * direction;
+
+	IObject *sphere = new SphereObject(resourceManager->getBullet());
+
+	Entity *newEntity = new Entity(sphere, makeVector3(position.x, position.y, position.z), ENTITY_BULLET);
+
+	//GLfloat bulletSpeed = 0.008f;
+	GLfloat max = std::max(std::abs(center.x), std::max(std::abs(center.y), std::abs(center.z)));
+
+	if (max != 0)
+	{
+		newEntity->setVelocity(makeVector3((center.x / max)*BulletSpeed,
+			(center.y / max)*BulletSpeed,
+			(center.z / max)*BulletSpeed));
+	}
+	else
+	{
+		cout << "patrzysz na srodek" << endl;
+		//chyba
+		newEntity->setVelocity(makeVector3(BulletSpeed, BulletSpeed, BulletSpeed));
+	}
+
+	renderSystem->getNewObjects()->push_back(newEntity);
+}
 
 
 void ArtificialIntelligence::moveSomewhere(Entity* entity, GLfloat speed)
 {
+	vec2 u = vec2(0.0f, 1.0f);
+	vec2 v = vec2(entity->getMovementDirection().x, entity->getMovementDirection().z);
+	GLfloat angle = -1 * 180 / pi<GLfloat>() * fmodf(atan2(u.x*v.y - v.x*u.y, u.x*v.x + u.y*v.y), 2 * pi<GLfloat>());
+	entity->setRotation(makeVector3(0.0f, angle, 0.0f));
 	entity->setVelocity(_returnSpeed);
 }
 
@@ -81,8 +132,6 @@ void ArtificialIntelligence::moveToCamera(Entity* entity, GLfloat speed)
 
 void ArtificialIntelligence::moveWhenCollision(Entity *entity, GLfloat speed)
 {
-
-
 	vec2 movementDir = - vec2(entity->getMovementDirection().x, entity->getMovementDirection().z);
 	vec2 oldVelocity = vec2(entity->getVelocity().x, entity->getVelocity().z);
 	GLfloat max = std::max(std::abs(movementDir.x), std::abs(movementDir.y)); //y to tak naprawde z
@@ -104,8 +153,6 @@ void ArtificialIntelligence::moveWhenCollision(Entity *entity, GLfloat speed)
 	GLfloat angle = -1 * 180 / pi<GLfloat>() * fmodf(atan2(u.x*v.y - v.x*u.y, u.x*v.x + u.y*v.y), 2 * pi<GLfloat>());
 	entity->setRotation(makeVector3(0.0f, entity->getRotation().y + angle, 0.0f));
 	_collisionTime = glfwGetTime();
-	//_returnSpeed = entity->getVelocity();
-
 
 	vec2 u2 = vec2(entity->getVelocity().x, entity->getVelocity().z);
 	GLfloat angle2 = rand() % 90 + 1;
@@ -116,4 +163,7 @@ void ArtificialIntelligence::moveWhenCollision(Entity *entity, GLfloat speed)
 	vec2 returnSpeed = -vec2(u2.x * cos(glm::radians(angle2)) - u2.y * sin(glm::radians(angle2)), u2.x * sin(glm::radians(angle2)) + u2.y * cos(glm::radians(angle2)));
 	_returnSpeed = makeVector3(returnSpeed.x, 0.0f, returnSpeed.y);
 	_resetRotation = true;
+
+
+
 }
--- Doom/ArtificialIntelligence.h
+++ Doom/ArtificialIntelligence.h
@@ -7,12 +7,13 @@
 #include "ResourceManager.h"
 #include "Entity.h"
 #include "CameraSystem.h"
+#include "RenderSystem.h"
+#include "SphereObject.h"
 // GLM Mathematics
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <GLFW/glfw3.h>
-
 
 using namespace std;
 using namespace glm;
@@ -24,7 +25,7 @@ private:
 	GLfloat _collisionTime;
 	Vector3 _returnSpeed;
 	bool _resetRotation;
-	
+	void shoot(Entity *entity);
 	//GLfloat _angle;
 
 	void moveToCamera(Entity* entity, GLfloat speed);
@@ -34,6 +35,7 @@ public:
 
 	void move(Entity* entity, GLfloat speed);
 	void moveWhenCollision(Entity *entity, GLfloat speed);
+
 
 	ArtificialIntelligence();
 	~ArtificialIntelligence();
